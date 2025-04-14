80424cb7410e772588353ba929e224c36dc6ab1b
Zxl-Elena <146520668+Zxl-Elena@users.noreply.github.com>
2025-04-14 01:51:08 +0000

    feat: post commit

--- routes/api.js
+++ routes/api.js
@@ -22,19 +22,18 @@ module.exports = function (app) {
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
       if (!title) {
-        res.json('missing required field title');
+        return res.json('missing required field title');
       }
 
       try {
         const newBook = new Book({ title });
         const savedBook = await newBook.save();
-        console.log(savedBook);
-        res.json({
+        return res.json({
           _id: savedBook._id,
           title: savedBook.title
         });
       } catch (err) {
-        res.status(500).json({ error: 'Cannot add new book' })
+        return res.status(500).json({ error: 'Cannot add new book' })
       }
     })
     
@@ -50,10 +49,29 @@ module.exports = function (app) {
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
     
-    .post(function(req, res){
+    .post(async (req, res) => {
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
+      if (!comment) {
+        return res.json('missing required field comment');
+      }
+
+      try {
+        const updatedBook = await Book.findByIdAndUpdate(
+          bookid,
+          {
+            $push: { comments: comment },
+            $inc: { commentcount: 1 }
+          },
+          {new: true}
+        )
+        if (!updatedBook) { return res.json('no book exists') };
+
+        return res.json({_id: updatedBook._id, title: updatedBook.title, comments: updatedBook.comments});
+      } catch(e) {
+        return res.json(e);
+      }
     })
     
     .delete(function(req, res){
